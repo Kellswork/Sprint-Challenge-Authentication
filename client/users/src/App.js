5aad18840989b43bcf7f9a48de0f14ac8564f990
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-07-27 09:14:18 +0100

    add react router to login and signup components

--- client/users/src/App.js
+++ client/users/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 import './App.css';
 import SignupForm from './Users/SignupForm';
 import LoginForm from './Users/LoginForm';
-import axiosAuth from './Users/axiosAuth';
-import DadJokeList from './DadJokes/DadjokesList';
 
 class App extends Component {
   state = {
@@ -60,23 +59,31 @@ class App extends Component {
   };
 
   render() {
-    console.log(this.state.dadJokes)
+    console.log(this.state.dadJokes);
     return (
       <div>
-        <SignupForm
-          onChange={this.onChangeHandler}
-          username={this.state.username}
-          password={this.state.password}
-          onSubmit={this.createuserWithAxios}
+        <Route
+          path="/signup"
+          render={() => (
+            <SignupForm
+              onChange={this.onChangeHandler}
+              username={this.state.username}
+              password={this.state.password}
+              onSubmit={this.createuserWithAxios}
+            />
+          )}
         />
-
-        <LoginForm
-          username={this.state.username}
-          password={this.state.password}
-          onSubmit={this.loginUserWithAxios}
-          onChange={this.onChangeHandler}
+        <Route
+          path="/login"
+          render={() => (
+            <LoginForm
+              username={this.state.username}
+              password={this.state.password}
+              onSubmit={this.loginUserWithAxios}
+              onChange={this.onChangeHandler}
+            />
+          )}
         />
-        <DadJokeList jokes={this.dadJokes} />
       </div>
     );
   }
